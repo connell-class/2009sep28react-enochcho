e8307ccfa6fd53f83b86d1d13d08256fa674384e
enochcho <enochcho97@hotmail.com>
2020-10-09 12:02:00 -0400

    Deleted custom test class

--- src/main/java/com/revature/eval/java/core/EvaluationService.java
+++ src/main/java/com/revature/eval/java/core/EvaluationService.java
@@ -633,7 +633,7 @@ public class EvaluationService {
 		
 		return prime;
 	}
-
+	
 	/**
 	 * 13 & 14. Create an implementation of the atbash cipher, an ancient encryption
 	 * system created in the Middle East.
